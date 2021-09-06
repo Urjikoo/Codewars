ba978655fc0ad38033ba7b44f52460a40a3b0eed
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-09-06 10:43:34 -0400

    push

--- reverse.js
+++ reverse.js
@@ -1,4 +1,4 @@
-// Complete the solution so that it reverses the string passed into it.
+// Complete the solution so that it reverses string passed into it.
 // 'world'  =>  'dlrow'
 
 function solution(str) {
