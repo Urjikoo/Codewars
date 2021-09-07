e6b8a1797947525f50e4e7e6714626a6413e6042
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-09-07 10:44:35 -0400

    push

--- return_string.js
+++ return_string.js
@@ -1,6 +1,6 @@
 // We need a function that can transform a number into a string.
 
-// What ways of achieving this do you know?
+// What ways of achieving this?
 
 function numberToString(num) {
   return num.toString();
