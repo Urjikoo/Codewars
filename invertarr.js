0c4270964a767a62a7ae9837cb8946ad37967d9d
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-10-07 22:46:24 -0400

    thurs

---
+++ invertarr.js
@@ -0,0 +1,9 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+function invert(array) {
+  var newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    newArr.push(-array[i]);
+  }
+  return newArr;
+}
