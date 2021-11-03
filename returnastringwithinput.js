7775cf3b0af845a23749bfe10029ba8124a0d5d1
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-11-03 12:02:32 -0400

    returnstring

---
+++ returnastringwithinput.js
@@ -0,0 +1,8 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//  we have a function called greet that returns a string "helllo.."
+// the function should return the string with the input name
+function greet(name) {
+  //your code here
+  name = `Hello, ${name} how are you doing today?`;
+  return name;
+}
