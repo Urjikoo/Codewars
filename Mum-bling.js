ad197206a982fb17707270063fb2e2771a400e32
Resilient Coder <urjig122@gmail.com>
2021-11-10 00:19:19 -0500

    codeWars_

---
+++ Mum-bling.js
@@ -0,0 +1,14 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+  // your code
+
+  return [...s]
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join("-");
+}
