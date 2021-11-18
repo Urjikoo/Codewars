8c1a478bf673512209fa05c24b16be5ef9d2af0c
Resilient Coder <urjig122@gmail.com>
2021-11-17 22:05:23 -0500

    codewars_

---
+++ unlimitedSum.js
@@ -0,0 +1,18 @@
+// Write a function sum that accepts an unlimited number of integer arguments, and adds all of them together.
+
+// The function should reject any arguments that are not integers, and sum the remaining integers.
+
+// sum(1, 2, 3)   // -> 6
+// sum(1, "2", 3) // -> 4
+
+function sum() {
+  //...
+
+  let newSum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "number" && arguments[i] % 1 === 0) {
+      newSum = newSum + arguments[i];
+    }
+  }
+  return Math.floor(newSum);
+}
