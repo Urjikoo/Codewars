2d0921d90099655d9e40b3df7f99009ba944199d
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-11-23 15:15:39 -0500

    add_nonempty_array

---
+++ add_non-empty_array.js
@@ -0,0 +1,13 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// param is an array;  declare a function that returns the result of multiplication of numbers; ex [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 ;
+// declare a function; declare a loop that iterates through the integers; use a method (reduce)
+
+function multiply() {
+  let arr = [2, 3, 4];
+  let result = arr.reduce((a, b) => a * b);
+  return result;
+}
+multiply();
