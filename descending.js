7b5b45c14691c3ef906d10b5dd3703b0a00f21d2
Resilient Coder <urjig122@gmail.com>
2021-12-14 12:44:08 -0500

    descendingA_

--- descending.js
+++ descending.js
@@ -1,5 +1,6 @@
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
 // Essentially, rearrange the digits to create the highest possible number.
+//
 // param is string; declaring a function return the string from the last to beginging.;ex. 45=> 54 ;declare a function that returns the decending order.>
 
 function descending(num) {
