d3e0146e58a8bae102c51ed8639e72894ce0fc8a
Urji <47441358+Urjikoo@users.noreply.github.com>
2021-12-25 22:19:59 -0500

    Update implementCar_OOP.js

--- implementCar_OOP.js
+++ implementCar_OOP.js
@@ -1,6 +1,6 @@
 // use a constructor function to implement a car. a car has a make and a speed property. The speed property is the current speed of the car in km/h;
 //  1mplement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console.
-// Implement a 'brake' method that will decrease the car's speed by 5 and log the new speed to the console'
+// Implement a 'brake' method that will decrease the car's speed by 5 and log  the new speed to the console'
 // create 2 car objects and experiments with calling 'accelerate' and 'brake' multiple times on each of them.
 // Data car 1=> 'BMW' going at 120 km/h
 // Data car2=>'Mercedes' going at 95 km/h
